ea527ffea71e100c988d0b7f1185700f9a1fa16e
Ariel Calzada <ariel.calzada@gmail.com>
2023-05-03 03:01:33 -0500

    - Add description into readme file - Implement the index route

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# python-proxy
+# Python Proxy
+
+Redirect request body, path, GET/POST methods and query string for one url to
+another and returns the json response.
--- main.py
+++ main.py
@@ -1,35 +1,53 @@
+import logging
+import os
 from pathlib import Path
 
 import dotenv
+import requests
 
 from flask import Flask
+from flask import jsonify
 from flask import request
 
 dotenv.load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
 
 base_dir = Path(__file__).resolve().parent
 
 app = Flask(__name__)
 
 
-@app.route("/<path:url>", methods=["GET", "POST"])
-def index():
-    header = request.headers.get("referer")
-    if not referer:
-        return Response(
-            "Relative URL sent without a a proxying request referal. Please specify a valid proxy host (/p/url)",
-            400,
-        )
-    proxy_ref = proxied_request_info(referer)
-    host = proxy_ref[0]
-    redirect_url = "/p/%s/%s%s" % (
-        host,
-        url,
-        (
-            "?" + request.query_string.decode("utf-8")
-            if request.query_string
-            else ""
-        ),
+@app.route("/<path:request_path>", methods=["GET", "POST"])
+def index(request_path):
+    query_string = request.query_string.decode("utf-8")
+    query_string_dict = dict(request.args)
+
+    real_url = os.environ["REAL_URL"]
+
+    logging.info(f"Request path: {request_path}")
+    logging.info(f"Query String: {query_string}")
+
+    url = real_url + "/" + request_path
+
+    kwargs = {
+        "url": url,
+        "params": query_string_dict,
+    }
+    response = requests.get(**kwargs)
+
+    return response.json()
+
+
+def main():
+    """Run the flask application if the main module is invoked directly."""
+    print("Running from main")
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
     )
-    LOG.debug("Redirecting relative path to one under proxy: %s", redirect_url)
-    return redirect(redirect_url)
+
+
+if __name__ == "__main__":
+    main()
